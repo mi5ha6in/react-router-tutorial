016c5991f391a24add3b49dfe1f92b69c20f8f16
mi5ha6in <mi5ha6in@gmail.com>
2022-12-27 00:21:04 +0300

    adds contacts

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
