23db4827c89a842ea172a6b5238f2b614de6c7d8
mi5ha6in <mi5ha6in@gmail.com>
2022-12-27 18:38:02 +0300

    adds the route for edit

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,9 @@ import Root, {
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
-import EditContact from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
 import "./index.css";
+import { action } from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
