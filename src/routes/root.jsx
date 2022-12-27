94b767a44218e70d7199fc7e4c786ccc2c961f4a
mi5ha6in <mi5ha6in@gmail.com>
2022-12-27 18:49:29 +0300

    adds the route for create new contacts

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,9 @@
-import { Link, Outlet, useLoaderData, Form } from "react-router-dom";
+import { Link, Outlet, useLoaderData, Form, redirect } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
