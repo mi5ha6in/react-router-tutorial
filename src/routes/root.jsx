53e1ffdcf3817ff5b6b6a892a7717969d00be4fc
mi5ha6in <mi5ha6in@gmail.com>
2022-12-27 21:17:40 +0300

    sync url and input

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,6 +7,7 @@ import {
   NavLink,
   useNavigation,
 } from "react-router-dom";
+import { useEffect } from "react";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -15,8 +16,13 @@ export async function action() {
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
+
   return (
     <>
       <div id="sidebar">
@@ -29,6 +35,7 @@ export default function Root() {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
@@ -81,5 +88,5 @@ export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
