70241101f01bffe9153e8cb9671f976ed4bc4798
mi5ha6in <mi5ha6in@gmail.com>
2022-12-28 13:10:11 +0300

    adds spiner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -20,6 +20,9 @@ export default function Root() {
   const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
   const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
 
   useEffect(() => {
     document.getElementById("q").value = q;
@@ -33,16 +36,17 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
               defaultValue={q}
               onChange={(event) => {
-                submit(event.currentTarget.form)
+                submit(event.currentTarget.form);
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
